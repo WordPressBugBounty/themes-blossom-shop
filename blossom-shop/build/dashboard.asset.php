77f3a40bd502bedd410fb730d39aad4ebcd07eb6
Sean Murphy <sean@iamseanmurphy.com>
2026-02-06 06:37:57 +0000

    1.2.2

--- blossom-shop/build/dashboard.asset.php
+++ blossom-shop/build/dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '6bbabf389311f8c6ab9d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'b7e54bf8b61c3c7ae45d');
